c05877fcfc8517a08f06a00a7447586c1651015e
Kevin-Zhouu <bigsoupzhou@gmail.com>
2022-09-11 01:20:03 +1000

    run

--- com.bash
+++ com.bash
@@ -2,7 +2,7 @@
 gcc -Wall -o program program.c
 
 echo "Testing data1.txt"
-./program a b c < data1.txt > test1out.txt
+./program a b c < data1.txt #> test1out.txt
 
 #echo "Testing data2.txt"
 #./program a b c < data2.txt
